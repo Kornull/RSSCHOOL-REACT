80c9fc9b53eebf0ea993faa0c1baf78454cd0dd1
Kornull <wolod1987kor@gmail.com>
2022-09-28 16:14:49 +0300

    fix: App

--- react-component/src/components/App/App.tsx
+++ react-component/src/components/App/App.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from '../../layout';
 
 class App extends Component {
   render(): React.ReactNode {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Layout />
+      </div>
+    );
   }
 }
 
