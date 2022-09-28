daf5623073002f46530fb0fa054d819db0b0a36e
Kornull <wolod1987kor@gmail.com>
2022-09-28 16:14:19 +0300

    feat: add layout

---
+++ react-component/src/layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class Layout extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
+}
+
+export default Layout;
