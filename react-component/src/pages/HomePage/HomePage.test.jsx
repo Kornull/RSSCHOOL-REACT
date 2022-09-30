9456ebd37df177d4bf25de3bf42e10d824f7c48d
Kornull <wolod1987kor@gmail.com>
2022-10-01 01:40:27 +0300

    fix: del comment

--- react-component/src/pages/HomePage/HomePage.test.jsx
+++ react-component/src/pages/HomePage/HomePage.test.jsx
@@ -1,7 +1,4 @@
 import './HomePage';
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import HomePage from './HomePage';
 
 test('real fetch call', async () => {
   const res = await fetch('https://rickandmortyapi.com/api/character/1');
