b7233b0f866d02ce97182d0ca4b3f143002f3c2c
Kornull <wolod1987kor@gmail.com>
2022-09-30 23:48:52 +0300

    feat: add cards to home page

--- react-component/src/pages/HomePage/HomePage.tsx
+++ react-component/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import Search from '../../components/Search';
+import Cards from '../../components/Cards/Cards';
+
+export type AboutCard = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin?: {
+    name: string;
+    url: string;
+  };
+  location?: {
+    name: string;
+    url: string;
+  };
+  image?: string;
+  episode?: string[];
+  url?: string;
+  created?: string;
+};
+
+type State = {
+  cards: AboutCard[];
+};
 
 class HomePage extends Component {
+  state: State = {
+    cards: [],
+  };
+
+  componentDidMount() {
+    fetch(
+      'https://rickandmortyapi.com/api/character/1,589,99,7,313,4,666,13,23,34,65,43,2,67,89,30'
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ cards: data }));
+  }
+
   render() {
     return (
       <>
-        <h1>HomePage</h1>
         <Search />
+        <Cards cardList={this.state.cards} />
       </>
     );
   }
