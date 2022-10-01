a53e3474a458d040de5a8c6392d1caf6d844fc61
Kornull <wolod1987kor@gmail.com>
2022-10-01 18:58:06 +0300

    feat: add style 404 page

--- react-component/src/pages/NotFoundPage/NotFoundPage.tsx
+++ react-component/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import NotFoundPng from '../../image/not-found.png';
+
+import styles from './NotFoundPage.module.scss';
 
 class NotFoundPage extends Component {
   render() {
     return (
-      <div>
-        <h1>404</h1>
-        <p>page not found</p>
+      <div className={styles.NotPage}>
+        <img className={styles.NotPageImg} src={NotFoundPng} alt="not-found" />
+        <p>Page not found</p>
         <p>Select an item from the menu for further viewing</p>
       </div>
     );
