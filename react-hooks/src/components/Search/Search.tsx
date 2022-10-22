3b46aac8638cc6eb1449554647f644f9e72d8081
Kornull <wolod1987kor@gmail.com>
2022-10-22 19:39:32 +0300

    fix: remove Search class, add hooks

--- react-hooks/src/components/Search/Search.tsx
+++ react-hooks/src/components/Search/Search.tsx
@@ -1,11 +1,7 @@
-import React, { Component, FormEvent } from 'react';
+import React, { FormEvent, useState, useEffect } from 'react';
 
 import IconSVG from '../../image/icon-search.svg';
 import styles from './Search.module.scss';
-
-type StateSearch = {
-  readonly search: string;
-};
 
 enum LocalStoreKey {
   keyStorage = 'SearchValues',
@@ -15,59 +11,53 @@ type SearchProps = {
   searchName: (name: string) => void;
 };
 
-class Search extends Component<SearchProps, StateSearch> {
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = {
-      search: '',
-    };
-  }
+const Search = (props: SearchProps): JSX.Element => {
+  const [search, setSearch] = useState('');
 
-  handelChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  const handelChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     event.preventDefault();
-    this.setState({ search: event.target.value });
+    setSearch(event.target.value);
   };
 
-  handleSearch = (ev: FormEvent) => {
+  const handleSearch = (ev: FormEvent): void => {
     ev.preventDefault();
-    this.props.searchName(this.state.search);
+    props.searchName(search);
   };
 
-  componentDidMount(): void {
+  //Loading search text from localStore
+  useEffect(() => {
     const value = localStorage.getItem(LocalStoreKey.keyStorage);
-    if (value) this.setState({ search: value });
-    localStorage.setItem(LocalStoreKey.keyStorage, '');
-  }
 
-  componentWillUnmount(): void {
-    const { search } = this.state;
-    localStorage.setItem(LocalStoreKey.keyStorage, search);
-  }
+    if (value) setSearch(value);
+    return localStorage.setItem(LocalStoreKey.keyStorage, '');
+  }, []);
 
-  render(): JSX.Element {
-    const { search } = this.state;
-    return (
-      <div className={styles.search}>
-        <form className={styles.searchForm} data-testid="form-search" onSubmit={this.handleSearch}>
-          <input
-            className={styles.searchInput}
-            data-testid="search-cards"
-            name="search"
-            type="text"
-            placeholder="Search..."
-            value={search}
-            autoComplete="off"
-            onChange={this.handelChange}
-          />
-          <button>
-            <svg className={styles.searchIcon} data-testid="button-search">
-              <use xlinkHref={`${IconSVG}#icon-search`} />
-            </svg>
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  //update search localStore
+  useEffect(() => {
+    return localStorage.setItem(LocalStoreKey.keyStorage, search);
+  }, [search]);
+
+  return (
+    <div className={styles.search}>
+      <form className={styles.searchForm} data-testid="form-search" onSubmit={handleSearch}>
+        <input
+          className={styles.searchInput}
+          data-testid="search-cards"
+          name="search"
+          type="text"
+          placeholder="Search..."
+          value={search}
+          autoComplete="off"
+          onChange={handelChange}
+        />
+        <button>
+          <svg className={styles.searchIcon} data-testid="button-search">
+            <use xlinkHref={`${IconSVG}#icon-search`} />
+          </svg>
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Search;
