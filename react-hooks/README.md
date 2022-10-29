cb083bf56a2bbc7f27e2140a8e1c7d40b7edf028
Kornull <96052707+Kornull@users.noreply.github.com>
2022-10-29 20:42:37 +0300

    fix: dell hook folder

--- react-hooks/README.md
+++
@@ -1,3 +0,0 @@
-This is a project by Kornull
-
-Description:
--- react-hooks/src/components/Form/FormInputs/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './InputFirstName';
-export * from './InputLastName';
-export * from './InputEmail';
-export * from './InputGender';
-export * from './Select';
-export * from './InputLoadFile';
-export * from './InputCheckbox';
