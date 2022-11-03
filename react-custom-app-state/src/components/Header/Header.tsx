a580bf0ca8de0202d83c62c4893b6fb9a8363c18
Kornull <wolod1987kor@gmail.com>
2022-11-03 11:31:15 +0300

    fix: header link

--- react-custom-app-state/src/components/Header/Header.tsx
+++ react-custom-app-state/src/components/Header/Header.tsx
@@ -6,22 +6,26 @@ import { useCardContext } from '../Hooks';
 
 const Header = () => {
   const { cards } = useCardContext();
+
+  const handleClick = () => {
+    cards.viewPersonCard = false;
+  };
   return (
     <header className={styles.header} data-testid="header-component">
       <div className={styles.headerContainer}>
         <nav>
           <ul className={styles.headerNavigate} data-testid="navigation-list">
-            <li>
+            <li onClick={handleClick}>
               <NavLink className={styles.headerPageLink} end to="/">
                 Main
               </NavLink>
             </li>
-            <li>
+            <li onClick={handleClick}>
               <NavLink className={styles.headerPageLink} end to="about">
                 About us
               </NavLink>
             </li>
-            <li>
+            <li onClick={handleClick}>
               <NavLink className={styles.headerPageLink} end to="user-form">
                 Form
               </NavLink>
